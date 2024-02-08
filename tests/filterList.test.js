f02e37b9054b64b7149e830d42073125501e83b6
tanishq <tanishq.shukla.cd.che21@iitbhu.ac.in>
2024-02-08 16:45:48 +0530

    testscript added

---
+++ tests/filterList.test.js
@@ -0,0 +1,10 @@
+const filterList = require('../src/filterList');
+
+test('throws an error if list length is not a multiple of 10', () => {
+    expect(() => filterList([1, 2, 3])).toThrow("List length is not a multiple of 10");
+});
+
+test('returns a list with items at positions multiple of 2 or 3 removed', () => {
+    expect(filterList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual([1, 5, 7]);
+    expect(filterList([...Array(20).keys()].map(n => n + 1))).toEqual([1, 5, 7, 11, 13, 17, 19]);
+});
